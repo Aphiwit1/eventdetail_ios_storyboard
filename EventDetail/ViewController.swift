7c2468acb77627b25de2f73ed16f565b50c0179f
Aphiwit's 2019 Boonprasert <aphiwit.2019@gmail.com>
2019-07-22 17:31:05 +0700

    Initial Commit

---
+++ EventDetail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EventDetail
+//
+//  Created by AphiwitSCB on 22/7/2562 BE.
+//  Copyright © 2562 OnceKB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
